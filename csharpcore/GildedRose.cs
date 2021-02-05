6807cb7a316d32f1bc74b21300963c3e49276959
Dylan Chapotte <Dylan.Chapotte@ext.cdiscount.com>
2021-02-05 10:16:41 +0100

    fixed test

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -14,7 +14,7 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (!Items[i].Name.Equals("Aged Brie") && !Items[i].Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
                 {
                     if (Items[i].Quality > 0)
                     {
